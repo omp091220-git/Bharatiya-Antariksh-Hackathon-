ee638a7a1f5975b9ec48c2c6828e27d4b3c3c9d0
Om! <omp091220@gmail.com>
2026-07-02 11:49:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 1400,
+    height: 900,
+    backgroundColor: '#050505',
+    webPreferences: { nodeIntegration: true }
+  });
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
+app.on('window-all-closed', () => { if (process.platform !== 'darwin') app.quit(); });
